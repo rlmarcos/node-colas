da2a3d44c3b2ca0b1a85c724278a544cc7bec16f
rlmarcos <werh79@gmail.com>
2019-05-01 20:43:59 +0200

    Primer commit

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,39 +1,55 @@
 const { io } = require('../server');
-const { TicketControl } = require('../classes/ticket-control');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
 
-const ticketControl = new TicketControl();
+const usuarios = new Usuarios();
 
 
 io.on('connection', (client) => {
 
-    client.on('siguienteTicket', (data, callback) => {
-        let siguiente = ticketControl.siguiente();
-        console.log(siguiente);
-        callback(siguiente);
+    client.on('entrarChat', (data, callback) => {
+
+        if( !data.nombre || !data.sala ){
+            return callback({
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
+            });
+        }
+
+        client.join(data.sala);
+
+        usuarios.agregarPersona( client.id, data.nombre, data.sala );
+
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala) );
+
+        callback( usuarios.getPersonasPorSala( data.sala ) );
+
     });
 
-    client.emit('estadoActual', {
-    	actual: ticketControl.getUltimoTicket(),
-        ultimos4: ticketControl.getUltimos4()
+    client.on('crearMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje( persona.nombre, data.mensaje );
+        client.broadcast.to(persona.sala).emit( 'crearMensaje', mensaje );
+
     });
 
-    client.on('atenderTicket', (data, callback) => {
+    client.on('disconnect', () => {
 
-    	if( !data.escritorio ){
-    		return callback({
-    			err: true,
-    			mensaje: 'El escritorio es necesario'
-    		});
-    	}
+        let personaBorrada = usuarios.borrarPersona( client.id );
 
-    	let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} salió`) );
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala) );
 
-    	callback(atenderTicket);
+    });
 
-    	// Actualizar / Notificar cambios en los ULTIMOS 4
-        client.broadcast.emit('ultimos4', {
-            ultimos4: ticketControl.getUltimos4()
-        });
+    // Mensajes privados
+    client.on('mensajePrivado', data => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        client.broadcast.to(data.para).emit( 'mensajePrivado', crearMensaje( persona.nombre, data.mensaje ) );
 
     });
 
